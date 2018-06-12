e67670f56b471ae396c0c2d41b6b6146c2cd0a0e
LEDUNOIS Simon <simon.ledunois@cgi.com>
2018-06-12 15:25:50 +0200

    Adding stage-2 babel preset, updating npm scripts and debugging app launching

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-AppRegistry.registerComponent('SchooltoringMobileApplication', () => App);
+AppRegistry.registerComponent('Schooltoring', () => App);
