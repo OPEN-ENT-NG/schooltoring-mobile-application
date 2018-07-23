37a113bd57f0db10aae4222c708ec319079a1e86
Quentin Rollin <quentin.rollin@cgi.com>
2018-07-23 10:42:12 +0200

    STMA-1 : Avatar in menu for Profile

--- src/containers/Navigator/Navigator.js
+++ src/containers/Navigator/Navigator.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { createBottomTabNavigator } from "react-navigation";
 
+import Icon from "react-native-vector-icons/MaterialIcons";
+
 import Home from "../Home/Home";
 import Requests from "../Requests/Requests";
 import Messages from "../Messages/Messages";
@@ -8,7 +10,7 @@ import Profile from "../Profile/Profile";
 
 import { COLORS } from "../../styles/common";
 import NavigationService from "../../api/Navigation";
-import Icon from "react-native-vector-icons/MaterialIcons";
+import Avatar from "../../components/Avatar/Avatar";
 
 const Navigator = createBottomTabNavigator(
   {
@@ -18,25 +20,28 @@ const Navigator = createBottomTabNavigator(
     Profile
   },
   {
-    navigationOptions: ({ navigation }) => ({
-      tabBarLabel: () => null,
-      tabBarIcon: () => {
-        const { routeName } = navigation.state;
-        let iconName;
-        if (routeName === "Home") {
-          iconName = "home";
-        } else if (routeName === "Requests") {
-          iconName = "help";
+    navigationOptions: ({ navigation }) => {
+      return {
+        tabBarLabel: () => null,
+        tabBarIcon: () => {
+          const { routeName } = navigation.state;
+          switch (routeName) {
+            case "Home": {
+              return <Icon name="home" color={COLORS.TEXT} size={25} />;
+            }
+            case "Requests": {
+              return <Icon name="help" color={COLORS.TEXT} size={25} />;
+            }
+            case "Messages": {
+              return <Icon name="chat" color={COLORS.TEXT} size={25} />;
+            }
+            case "Profile": {
+              return <Avatar color={COLORS.TEXT} size={30} />;
+            }
+          }
         }
-        if (routeName === "Messages") {
-          iconName = "chat";
-        }
-        if (routeName === "Profile") {
-          iconName = "face";
-        }
-        return <Icon name={iconName} color={COLORS.TEXT} size={25} />;
-      }
-    })
+      };
+    }
   }
 );
 
