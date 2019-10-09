0b83063a22713d5312dfa8d13635db8f4b6b3896
ROLLIN Quentin <quentin.rollin@cgi.com>
2019-10-09 10:14:51 +0200

    :bug: remove english translation

--- src/api/I18n.js
+++ src/api/I18n.js
@@ -1,9 +1,8 @@
 import I18n from "i18n-js";
 import * as RNLocalize from "react-native-localize";
-import en from "../locales/en";
 import fr from "../locales/fr";
 
-I18n.translations = { fr, en };
+I18n.translations = { fr };
 
 const languageTag =
   RNLocalize.findBestAvailableLanguage(Object.keys(I18n.translations))
