d26cfbed0aa171e00acb356ffeea57a4178a2899
ROLLIN Quentin <quentin.rollin@cgi.com>
2019-10-23 14:11:01 +0200

    :bug: crash on ipad release startup

--- src/api/I18n.js
+++ src/api/I18n.js
@@ -1,12 +1,9 @@
 import I18n from "i18n-js";
-import * as RNLocalize from "react-native-localize";
 import fr from "../locales/fr";
 
 I18n.translations = { fr };
 
-const languageTag =
-  RNLocalize.findBestAvailableLanguage(Object.keys(I18n.translations))
-    .languageTag || "fr";
+const languageTag = "fr";
 
 I18n.locale = languageTag;
 
